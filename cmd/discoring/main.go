5e3c273a6b116fece37a4463a580a5eda7aa6043
kirbx01 <rpriyanshi434@gmail.com>
2026-09-19 00:26:15 +0530

    bootstrap the bot from env config

---
+++ cmd/discoring/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+
+	"discoring/internal/bot"
+)
+
+func main() {
+	godotenv.Load()
+	token := os.Getenv("DISCORD_TOKEN")
+	if token == "" {
+		log.Fatal("DISCORD_TOKEN not set")
+	}
+
+	s, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Identify.Intents = discordgo.IntentsGuilds |
+		discordgo.IntentsGuildVoiceStates |
+		discordgo.IntentsGuildMessages |
+		discordgo.IntentsMessageContent
+
+	s.AddHandler(func(s *discordgo.Session, _ *discordgo.Ready) {
+		bot.RegisterCommands(s)
+		log.Println("discoring online, audio is crisp")
+	})
+	s.AddHandler(bot.OnInteraction)
+	s.AddHandler(bot.OnMessage)
+
+	if err := s.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer s.Close()
+	s.UpdateGameStatus(0, "!play | /play")
+	select {}
+}
